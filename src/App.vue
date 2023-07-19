90f97d277880f043af1ca80c5f0e59566c58bf70
akhialomgir <akhialomgir362856@gmail.com>
2023-07-19 17:05:24 +0800

    Enable persistent storage of dark theme

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,18 @@
 
 <script setup lang="ts">
 import ToolBar from '@/components/ToolBar.vue'
+import { useToolBarStore } from '@/store/toolbar'
+import { onMounted } from 'vue'
+
+const store = useToolBarStore()
+const { dark } = store;
+
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+
+onMounted(() => {
+  theme.global.name.value = dark ? 'light' : 'dark'
+});
 </script>
 
 <style lang="scss">
