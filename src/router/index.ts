8dc3e4d1f65435e9f7b126ba0ae8581bdc2681a9
Frog Chen <satgo1546@yeah.net>
2023-07-21 07:57:49 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,28 +3,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/app/',
+    path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
         path: '',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () => import('@/views/Home.vue'),
       },
       {
         path: 'login',
         name: 'Login',
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Login.vue'),
+        component: () => import('@/views/Login.vue'),
       },
     ],
   },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/EYE-web'),
   routes,
 })
 
